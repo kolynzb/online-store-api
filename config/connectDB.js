8980f0f250d78d7cbb103bac9678e043fd834a3d
kolynzb <collinsbenda360@gmail.com>
2022-03-10 10:38:44 +0300

    The First Commit

---
+++ config/connectDB.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const {NODE_ENV,DB_PROD,DB_LOCAL,DB_TEST} = process.env
+
+const DB_URI =""
+
+switch (NODE_ENV) {
+    case 'development':
+        DB_URI = DB_DEV
+        break;
+        case 'test':
+            DB_URI = DB_TEST
+            break;
+        case 'development':
+                DB_URI = DB_PROD
+                break;
+    default:
+        DB_URI = DB_LOCAL
+        break;
+    
+}
+
+NODE_ENV === 'production' ? DB_PROD : DB_LOCAL;
+
+const connectDB = async ()=>{
+ await mongoose.connect(DB_URI).then(()=>console.log('Connected successfully')).catch(err=>console.log('Error connecting',err))
+}
+
+module.exports = connectDB;
