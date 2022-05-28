4627c5c3c275b48c323398ee28ac04cc3c459573
kolynzb <collinsbenda360@gmail.com>
2022-05-28 06:45:42 +0300

    Modify Cart Schema

--- src/models/cart.model.js
+++ src/models/cart.model.js
@@ -17,8 +17,15 @@ const CartSchema = new mongoose.Schema(
           type: Number,
           default: 1,
         },
+        price: {
+          type: Number,
+        },
       },
     ],
+    totalPrice: {
+      // TODO: Make this a virtual schema
+      type: Number,
+    },
   },
   { timestamps: true }
 );
